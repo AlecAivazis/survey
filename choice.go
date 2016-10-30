032208897edaf82091c2f47f30769d0a767663db
Alec Aivazis <alec@aivazis.com>
2016-10-30 11:23:03 -0700

    choice now responds to user input

--- choice.go
+++ choice.go
@@ -1,26 +1,33 @@
 package probe
 
 import (
+	"errors"
+	"fmt"
 	tm "github.com/buger/goterm"
 	"time"
 )
 
+// Choice is a prompt that presents a
 type Choice struct {
 	Question string
 	Choices  []string
 }
 
 func (prompt *Choice) Prompt() (string, error) {
+	// ask the question
+	fmt.Println(FormatAsk(prompt.Question))
+
 	// get the current location of the cursor
 	loc, err := CursorLocation()
 	// if something went wrong
 	if err != nil {
+		// TODO: don't panic but quit better
 		// yell loudly
 		panic(err)
 	}
 
 	// the height of the prompt's output
-	height := 2
+	height := 3
 
 	// the starting point of the list depends on wether or not we
 	// are at the bottom of the current terminal session
@@ -36,15 +43,28 @@ func (prompt *Choice) Prompt() (string, error) {
 	}
 
 	for {
+		// wait for an input from the user
+		ascii, keyCode, err := getChar()
+		// if there is an error
+		if err != nil {
+			// bubble up
+			return "", err
+		}
 
+		// if the user sends SIGTERM (3) or presses esc (27)
+		if ascii == 3 || ascii == 27 {
+			// hard close
+			return "", errors.New("Goodbye.")
+		}
+
+		tm.Print(ascii, keyCode, err, "\n")
 		tm.Print("Current Time: ", time.Now().Format(time.RFC1123))
 		tm.Print("\nHello")
 
 		tm.Flush() // Call it every time at the end of rendering
-		// make sure we overwrite the first line
-		tm.MoveCursor(initialLocation, 1)
 
-		time.Sleep(time.Second)
+		// make sure we overwrite the first line next time we print
+		tm.MoveCursor(initialLocation, 1)
 	}
 
 	return "hello", nil
---
+++ getChar.go
@@ -0,0 +1,42 @@
+package probe
+
+import "github.com/pkg/term"
+
+// Returns either an ascii code, or (if input is an arrow) a Javascript key code.
+func getChar() (ascii int, keyCode int, err error) {
+	t, _ := term.Open("/dev/tty")
+	term.RawMode(t)
+	bytes := make([]byte, 3)
+
+	var numRead int
+	numRead, err = t.Read(bytes)
+	if err != nil {
+		return
+	}
+	if numRead == 3 && bytes[0] == 27 && bytes[1] == 91 {
+		// Three-character control sequence, beginning with "ESC-[".
+
+		// Since there are no ASCII codes for arrow keys, we use
+		// Javascript key codes.
+		if bytes[2] == 65 {
+			// Up
+			keyCode = 38
+		} else if bytes[2] == 66 {
+			// Down
+			keyCode = 40
+		} else if bytes[2] == 67 {
+			// Right
+			keyCode = 39
+		} else if bytes[2] == 68 {
+			// Left
+			keyCode = 37
+		}
+	} else if numRead == 1 {
+		ascii = int(bytes[0])
+	} else {
+		// Two characters read??
+	}
+	t.Restore()
+	t.Close()
+	return
+}
