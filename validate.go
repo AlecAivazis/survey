ccbebebc6ef96b008e9e7ca4fca1ae87a1b71aa4
Alec Aivazis <alec@aivazis.com>
2016-12-16 01:03:26 -0800

    added min/max length validators

--- validate.go
+++ validate.go
@@ -2,8 +2,10 @@ package survey
 
 import (
 	"errors"
+	"fmt"
 )
 
+// Required does not allow an empty value
 func Required(str string) error {
 	// if the string is empty
 	if str == "" {
@@ -12,4 +14,30 @@ func Required(str string) error {
 	}
 	// nothing was wrong
 	return nil
+}
+
+// MaxLength requires that the string is no longer than the specified value
+func MaxLength(length int) Validator {
+	// return a validator that checks the length of the string
+	return func(str string) error {
+		// if the string is longer than the given value
+		if len(str) > length {
+			return fmt.Errorf("Value is too long. Max length is %v.", length)
+		}
+		// the input is fine
+		return nil
+	}
+}
+
+// MinLength requires that the string is longer or equal in length to the specified value
+func MinLength(length int) Validator {
+	// return a validator that checks the length of the string
+	return func(str string) error {
+		// if the string is longer than the given value
+		if len(str) < length {
+			return fmt.Errorf("Value is too short. Min length is %v.", length)
+		}
+		// the input is fine
+		return nil
+	}
 }
--- validate_test.go
+++ validate_test.go
@@ -1,6 +1,9 @@
 package survey
 
-import "testing"
+import (
+	"math/rand"
+	"testing"
+)
 
 func TestRequiredCanSucceed(t *testing.T) {
 	// a string to test
@@ -18,5 +21,33 @@ func TestRequiredCanFail(t *testing.T) {
 	if notValid := Required(str); notValid == nil {
 		//
 		t.Error("Non null did not return an error when one was expected.")
+	}
+}
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func randString(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Int63()%int64(len(letterBytes))]
+	}
+	return string(b)
+}
+
+func TestMaxLength(t *testing.T) {
+	// the string to test
+	testStr := randString(150)
+	// validate the string
+	if err := MaxLength(140)(testStr); err == nil {
+		t.Error("No error returned with input greater than 150 characters.")
+	}
+}
+
+func TestMinLength(t *testing.T) {
+	// the string to test
+	testStr := randString(10)
+	// validate the string
+	if err := MinLength(12)(testStr); err == nil {
+		t.Error("No error returned with input less than 12 characters.")
 	}
 }
