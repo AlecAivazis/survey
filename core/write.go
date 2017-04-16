27742687b656e6765667e6caf9679732b72c5da9
alec aivazis <alec@aivazis.com>
2017-04-15 23:42:33 -0700

    added basic panic handling to write

--- core/write.go
+++ core/write.go
@@ -1,1 +1,33 @@
 package core
+
+import (
+	"errors"
+	"reflect"
+)
+
+// Write takes a value and copies it to the target
+func Write(t interface{}, v interface{}) (err error) {
+	// when we're done
+	defer func() {
+		// if there was a panic
+		if r := recover(); r != nil {
+			// pass the panic on as an error
+			err = r.(error)
+		}
+	}()
+
+	// the target to write to
+	target := reflect.ValueOf(t)
+	value := reflect.ValueOf(v)
+
+	// make sure we were handed a point
+	if target.Kind() != reflect.Ptr {
+		return errors.New("you must pass a pointer as the target of a Write operation")
+	}
+
+	// set the value of the object we're pointing to to match the value
+	target.Elem().SetBool(value.Bool())
+
+	// nothing went wrong
+	return nil
+}
--- core/write_test.go
+++ core/write_test.go
@@ -1,1 +1,42 @@
 package core
+
+import (
+	"testing"
+)
+
+func TestWrite_returnsErrorIfTargetNotPtr(t *testing.T) {
+	// try to copy a value to a non-pointer
+	err := Write(true, true)
+	// make sure there was an error
+	if err == nil {
+		t.Error("Did not encounter error when writing to non-pointer.")
+	}
+}
+
+func TestWrite_canWriteToBool(t *testing.T) {
+	// a pointer to hold the boolean value
+	ptr := true
+
+	// try to copy a false value to the pointer
+	Write(&ptr, false)
+
+	// if the value is try
+	if ptr {
+		// the test failed
+		t.Error("Could not write a false bool to a pointer")
+	}
+}
+
+func TestWrite_recoversInvalidReflection(t *testing.T) {
+	// a variable to mutate
+	ptr := false
+
+	// write a boolean value to the string
+	err := Write(&ptr, "hello")
+
+	// if there was no error
+	if err == nil {
+		// the test failed
+		t.Error("Did not encounter error when forced invalid write.")
+	}
+}
