3b08888d7b8124f354a430e473f1cd0f14cc81b0
alec aivazis <alec@aivazis.com>
2017-04-16 00:16:36 -0700

    can write boolean and string

--- core/write.go
+++ core/write.go
@@ -7,15 +7,6 @@ import (
 
 // Write takes a value and copies it to the target
 func Write(t interface{}, v interface{}) (err error) {
-	// when we're done
-	defer func() {
-		// if there was a panic
-		if r := recover(); r != nil {
-			// pass the panic on as an error
-			err = r.(error)
-		}
-	}()
-
 	// the target to write to
 	target := reflect.ValueOf(t)
 	value := reflect.ValueOf(v)
@@ -25,9 +16,47 @@ func Write(t interface{}, v interface{}) (err error) {
 		return errors.New("you must pass a pointer as the target of a Write operation")
 	}
 
-	// set the value of the object we're pointing to to match the value
-	target.Elem().SetBool(value.Bool())
+	// handle the target based on its prop
+	switch target.Elem().Kind() {
+	// if we are writing to a string
+	case reflect.String:
+		err = writeString(target.Elem(), value)
+	// if we are writing to a bool
+	case reflect.Bool:
+		err = writeBool(target.Elem(), value)
+	}
+
+	// we're done
+	return err
+}
+
+func writeBool(target, source reflect.Value) (err error) {
+	// make sure we handle the source type
+	switch source.Kind() {
+	// if we are turning a boolean into a boolean
+	case reflect.Bool:
+		// just copy the boolean over
+		target.SetBool(source.Bool())
+	// otherwise its a source we do not recognize
+	default:
+		err = errors.New("Cannot convert to bool")
+	}
+	// nothing went wrong
+	return err
+}
+
+func writeString(target, source reflect.Value) (err error) {
+	// make sure we handle the source type
+	switch source.Kind() {
+	// if we are turning a string into a string
+	case reflect.String:
+		// just copy the string over
+		target.SetString(source.String())
+	// otherwise its a source we do not recognize
+	default:
+		err = errors.New("Cannot convert to string")
+	}
 
 	// nothing went wrong
-	return nil
+	return err
 }
--- core/write_test.go
+++ core/write_test.go
@@ -20,23 +20,51 @@ func TestWrite_canWriteToBool(t *testing.T) {
 	// try to copy a false value to the pointer
 	Write(&ptr, false)
 
-	// if the value is try
+	// if the value is true
 	if ptr {
 		// the test failed
 		t.Error("Could not write a false bool to a pointer")
 	}
 }
 
-func TestWrite_recoversInvalidReflection(t *testing.T) {
-	// a variable to mutate
-	ptr := false
+func TestWrite_canWriteString(t *testing.T) {
+	// a pointer to hold the boolean value
+	ptr := ""
 
-	// write a boolean value to the string
+	// try to copy a false value to the pointer
 	err := Write(&ptr, "hello")
+	if err != nil {
+		t.Error(err)
+	}
 
-	// if there was no error
-	if err == nil {
-		// the test failed
-		t.Error("Did not encounter error when forced invalid write.")
+	// if the value is not what we wrote
+	if ptr != "hello" {
+		t.Error("Could not write a string value to a pointer")
 	}
 }
+
+func TestWrite_gracefullyHandlesFailedStringWrites(t *testing.T) {
+	// a pointer to hold the boolean value
+	ptr := ""
+	// try to copy a false value to the pointer
+	err := Write(&ptr, false)
+	// if the value is try
+	if err == nil {
+		// the test failed
+		t.Error("Did not encouner error when casting boolean to string")
+	}
+}
+
+// func TestWrite_recoversInvalidReflection(t *testing.T) {
+// 	// a variable to mutate
+// 	ptr := false
+
+// 	// write a boolean value to the string
+// 	err := Write(&ptr, "hello")
+// 	fmt.Println(err.Error())
+// 	// if there was no error
+// 	if err == nil {
+// 		// the test failed
+// 		t.Error("Did not encounter error when forced invalid write.")
+// 	}
+// }
