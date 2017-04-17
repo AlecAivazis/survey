ba1f44715c84e559a8b4acebc930f0b5e06242a5
alec aivazis <alec@aivazis.com>
2017-04-16 21:36:39 -0700

    added test for confirm

---
+++ tests/confirm.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"github.com/alecaivazis/survey"
+	"github.com/alecaivazis/survey/tests/util"
+)
+
+var answer = false
+
+var goodTable = []TestUtil.TestTableEntry{
+	{
+		"Enter 'yes'", &survey.Confirm{
+			Message: "yes:",
+		}, &answer,
+	},
+	{
+		"Enter 'no'", &survey.Confirm{
+			Message: "yes:",
+		}, &answer,
+	},
+	{
+		"default", &survey.Confirm{
+			Message: "yes:",
+			Default: true,
+		}, &answer,
+	},
+	{
+		"not recognized (enter random letter)", &survey.Confirm{
+			Message: "yes:",
+			Default: true,
+		}, &answer,
+	},
+}
+
+func main() {
+	TestUtil.RunTable(goodTable)
+}
