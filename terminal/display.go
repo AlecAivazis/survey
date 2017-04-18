9358f198a1dd17637bd6b02d494318be5e042894
Cory Bennett <cbennett@netflix.com>
2017-04-18 00:19:23 -0700

    remove redundant declarations

--- terminal/display.go
+++ terminal/display.go
@@ -3,7 +3,7 @@ package terminal
 type EraseLineMode int
 
 const (
-	ERASE_LINE_END   EraseLineMode = iota
-	ERASE_LINE_START EraseLineMode = iota
-	ERASE_LINE_ALL   EraseLineMode = iota
+	ERASE_LINE_END EraseLineMode = iota
+	ERASE_LINE_START
+	ERASE_LINE_ALL
 )
