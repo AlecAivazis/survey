92d1133c39fe75859e1de8fda323145166be0d06
Cory Bennett <github@corybennett.org>
2017-05-09 11:59:19 -0700

    add RuneReader.ReadLine(mask) routine for consistent prompt handling. remove usage of bufio.Scanner

--- password.go
+++ password.go
@@ -1,9 +1,7 @@
 package survey
 
 import (
-	"fmt"
 	"os"
-	"unicode"
 
 	"github.com/AlecAivazis/survey/core"
 	"github.com/AlecAivazis/survey/terminal"
@@ -34,35 +32,7 @@ func (p *Password) Prompt() (line interface{}, err error) {
 	rr := terminal.NewRuneReader(os.Stdin)
 	rr.SetTermMode()
 	defer rr.RestoreTermMode()
-
-	pass := []rune{}
-	for {
-		r, _, _ := rr.ReadRune()
-		if r == '\r' || r == '\n' {
-			terminal.Print("\r\n")
-			break
-		}
-		if r == terminal.KeyInterrupt {
-			return "", fmt.Errorf("interrupt")
-		}
-		if r == terminal.KeyEndTransmission {
-			break
-		}
-		// allow for backspace/delete editing of password
-		if r == terminal.KeyBackspace || r == terminal.KeyDelete {
-			if len(pass) > 0 {
-				pass = pass[:len(pass)-1]
-				terminal.CursorBack(1)
-				terminal.EraseLine(terminal.ERASE_LINE_END)
-			}
-			continue
-		}
-		if unicode.IsPrint(r) {
-			pass = append(pass, r)
-			terminal.Print("*")
-		}
-	}
-	return string(pass), err
+	return rr.ReadLine('*')
 }
 
 // Cleanup hides the string with a fixed number of characters.
