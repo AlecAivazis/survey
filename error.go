f268d6f2ae72d7da2aca924fcebcd605ad1425cc
Equim <sayaka@ekyu.moe>
2017-08-21 02:36:22 +0800

    add error.go, add InterruptErr

---
+++ error.go
@@ -0,0 +1,9 @@
+package survey
+
+import (
+	"github.com/AlecAivazis/survey/terminal"
+)
+
+var (
+	InterruptErr = terminal.InterruptErr
+)
--- select.go
+++ select.go
@@ -2,7 +2,6 @@ package survey
 
 import (
 	"errors"
-	"fmt"
 	"os"
 
 	"github.com/AlecAivazis/survey/core"
@@ -170,7 +169,7 @@ func (s *Select) Prompt() (interface{}, error) {
 			break
 		}
 		if r == terminal.KeyInterrupt {
-			return "", fmt.Errorf("interrupt")
+			return "", InterruptErr
 		}
 		if r == terminal.KeyEndTransmission {
 			break
