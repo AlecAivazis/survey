5702183902fd4b9689f9251824fcd556c7761609
Alec Aivazis <alec@aivazis.com>
2019-05-30 16:37:15 -0700

    added test for improved enter behavior on selects

--- select_test.go
+++ select_test.go
@@ -274,6 +274,30 @@ func TestSelectPrompt(t *testing.T) {
 			},
 			"green",
 		},
+		{
+			"Test Select prompt with answers filtered out",
+			&Select{
+				Message: "Choose a color:",
+				Options: []string{"red", "blue", "green"},
+			},
+			func(c *expect.Console) {
+				c.ExpectString("Choose a color:")
+				// filter away everything
+				c.SendLine("z")
+				// send enter (should get ignored since there are no answers)
+				c.ExpectEOF()
+
+				// remove the filter we just applied
+				c.SendLine(string(terminal.KeyBackspace))
+
+				// apply a different filter
+				c.SendLine("b")
+				// press enter
+				c.ExpectEOF()
+
+			},
+			"blue",
+		},
 	}
 
 	for _, test := range tests {
