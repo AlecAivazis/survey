a4c302bc491d9d27c5062ec445e0e98ab3a7f98c
Ahmet Alp Balkan <ahmetb@google.com>
2019-06-02 21:47:26 -0700

    Update tests
    Signed-off-by: Ahmet Alp Balkan <ahmetb@google.com>

--- survey_test.go
+++ survey_test.go
@@ -151,7 +151,7 @@ func TestAsk(t *testing.T) {
 				c.SendLine("")
 
 				// Select
-				c.ExpectString("Choose a color:  [Use arrows to move, space to select, type to filter]")
+				c.ExpectString("Choose a color:  [Use arrows to move, type to filter]")
 				c.SendLine("yellow")
 				c.ExpectEOF()
 			},
