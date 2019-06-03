77696746c95298cca32710c7e24939aa8c0b6bc3
Carey Hoffman <cgchoffman@gmail.com>
2019-06-03 11:17:21 -0700

    Use Posix term Reader.  Windows Reader fails with Pexpect input

--- terminal/runereader_windows.go
+++ terminal/runereader_windows.go
@@ -1,6 +1,8 @@
 package terminal
 
 import (
+	"bufio"
+	"fmt"
 	"os"
 	"syscall"
 	"unsafe"
@@ -48,10 +50,13 @@ type keyEventRecord struct {
 
 type runeReaderState struct {
 	term uint32
+	buf  *bufio.Reader
 }
 
 func newRuneReaderState(input *os.File) runeReaderState {
-	return runeReaderState{}
+	return runeReaderState{
+		buf: bufio.NewReader(input),
+	}
 }
 
 func (rr *RuneReader) SetTermMode() error {
@@ -81,50 +86,34 @@ func (rr *RuneReader) RestoreTermMode() error {
 }
 
 func (rr *RuneReader) ReadRune() (rune, int, error) {
-	ir := &inputRecord{}
-	bytesRead := 0
-	for {
-		rv, _, e := readConsoleInput.Call(rr.Input.Fd(), uintptr(unsafe.Pointer(ir)), 1, uintptr(unsafe.Pointer(&bytesRead)))
-		// windows returns non-zero to indicate success
-		if rv == 0 && e != nil {
-			return 0, 0, e
-		}
-
-		if ir.eventType != EVENT_KEY {
-			continue
-		}
-
-		// the event data is really a c struct union, so here we have to do an usafe
-		// cast to put the data into the keyEventRecord (since we have already verified
-		// above that this event does correspond to a key event
-		key := (*keyEventRecord)(unsafe.Pointer(&ir.event[0]))
-		// we only care about key down events
-		if key.bKeyDown == 0 {
-			continue
-		}
-		if key.wdControlKeyState&(LEFT_CTRL_PRESSED|RIGHT_CTRL_PRESSED) != 0 && key.unicodeChar == 'C' {
-			return KeyInterrupt, bytesRead, nil
-		}
-
-		// not a normal character so look up the input sequence from the
-		// virtual key code mappings (VK_*)
-		if key.unicodeChar == 0 {
-			switch key.wVirtualKeyCode {
-			case VK_DOWN:
-				return KeyArrowDown, bytesRead, nil
-			case VK_LEFT:
-				return KeyArrowLeft, bytesRead, nil
-			case VK_RIGHT:
-				return KeyArrowRight, bytesRead, nil
-			case VK_UP:
-				return KeyArrowUp, bytesRead, nil
-			default:
-				// not a virtual key that we care about so just continue on to
-				// the next input key
-				continue
-			}
-		}
-		r := rune(key.unicodeChar)
-		return r, bytesRead, nil
+	r, size, err := rr.state.buf.ReadRune()
+	if err != nil {
+		return r, size, err
 	}
+	// parse ^[ sequences to look for arrow keys
+	if r == '\033' {
+		r, size, err = rr.state.buf.ReadRune()
+		if err != nil {
+			return r, size, err
+		}
+		if r != '[' {
+			return r, size, fmt.Errorf("Unexpected Escape Sequence: %q", []rune{'\033', r})
+		}
+		r, size, err = rr.state.buf.ReadRune()
+		if err != nil {
+			return r, size, err
+		}
+		switch r {
+		case 'D':
+			return KeyArrowLeft, 1, nil
+		case 'C':
+			return KeyArrowRight, 1, nil
+		case 'A':
+			return KeyArrowUp, 1, nil
+		case 'B':
+			return KeyArrowDown, 1, nil
+		}
+		return r, size, fmt.Errorf("Unknown Escape Sequence: %q", []rune{'\033', '[', r})
+	}
+	return r, size, err
 }
