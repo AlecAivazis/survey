e3e828f55c2348296b352b1aece53b8c77a5c6de
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2020-10-09 08:35:18 -0300

    (feat): usage example

---
+++ examples/inputfilesuggestion.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"path/filepath"
+
+	"github.com/AlecAivazis/survey/v2"
+)
+
+func suggestFiles(toComplete string) []string {
+	files, _ := filepath.Glob(toComplete + "*")
+	return files
+}
+
+// the questions to ask
+var q = []*survey.Question{
+	{
+		Name: "file",
+		Prompt: &survey.Input{
+			Message: "Which file should be read?",
+			Suggest: suggestFiles,
+			Help:    "Any file, may not even exit yet",
+		},
+		Validate: survey.Required,
+	},
+}
+
+func main() {
+	answers := struct {
+		File string
+	}{}
+
+	// ask the question
+	err := survey.Ask(q, &answers)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	// print the answers
+	fmt.Printf("File chosen %s.\n", answers.File)
+}
