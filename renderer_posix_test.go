392c4062b05ddb9f95730a3f5cca07ac6255846a
Mislav Marohnić <git@mislav.net>
2020-12-17 19:39:19 +0100

    Use old name of `creack/pty` since it was already vendored

--- renderer_posix_test.go
+++ renderer_posix_test.go
@@ -8,7 +8,7 @@ import (
 	"testing"
 
 	"github.com/AlecAivazis/survey/v2/terminal"
-	pseudotty "github.com/creack/pty"
+	pseudotty "github.com/kr/pty"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
