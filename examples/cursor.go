726e7cbd78944dfb20e0907ae4cfa89331246c58
David Martin <13771411+dmartin35@users.noreply.github.com>
2021-04-08 11:31:35 +0200

    Add option to show/hide cursor when prompting user (#345)

---
+++ examples/cursor.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/AlecAivazis/survey/v2"
+)
+
+// the questions to ask
+var simpleQs = []*survey.Question{
+	{
+		Name: "name",
+		Prompt: &survey.Input{
+			Message: "What is your name?",
+		},
+		Validate: survey.Required,
+	},
+}
+
+func main() {
+	ansmap := make(map[string]interface{})
+
+	// ask the question
+	err := survey.Ask(simpleQs, &ansmap, survey.WithShowCursor(true))
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	// print the answers
+	fmt.Printf("Your name is %s.\n", ansmap["name"])
+}
