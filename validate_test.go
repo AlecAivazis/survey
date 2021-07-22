3fff19a4f37ba02ceaf3e52c984bdcd6b73ac801
Mateo Aguirre Duque <52325575+MAD-py@users.noreply.github.com>
2021-07-22 00:51:40 -0500

    Validators for the number of responses in multiselect (#362)
    * :sparkles: validators for the number of responses in multiselect
    
    * documentation for MaxItems and MinItems validators

--- validate_test.go
+++ validate_test.go
@@ -3,6 +3,8 @@ package survey
 import (
 	"math/rand"
 	"testing"
+
+	"github.com/AlecAivazis/survey/v2/core"
 )
 
 func TestRequired_canSucceedOnPrimitiveTypes(t *testing.T) {
@@ -84,6 +86,40 @@ func randString(n int) string {
 		b[i] = letterBytes[rand.Int63()%int64(len(letterBytes))]
 	}
 	return string(b)
+}
+
+func TestMaxItems(t *testing.T) {
+	// the list to test
+	testList := []core.OptionAnswer{
+		core.OptionAnswer{Value: "a", Index: 0},
+		core.OptionAnswer{Value: "b", Index: 1},
+		core.OptionAnswer{Value: "c", Index: 2},
+		core.OptionAnswer{Value: "d", Index: 3},
+		core.OptionAnswer{Value: "e", Index: 4},
+		core.OptionAnswer{Value: "f", Index: 5},
+	}
+
+	// validate the list
+	if err := MaxItems(4)(testList); err == nil {
+		t.Error("No error returned with input greater than 6 items.")
+	}
+}
+
+func TestMinItems(t *testing.T) {
+	// the list to test
+	testList := []core.OptionAnswer{
+		core.OptionAnswer{Value: "a", Index: 0},
+		core.OptionAnswer{Value: "b", Index: 1},
+		core.OptionAnswer{Value: "c", Index: 2},
+		core.OptionAnswer{Value: "d", Index: 3},
+		core.OptionAnswer{Value: "e", Index: 4},
+		core.OptionAnswer{Value: "f", Index: 5},
+	}
+
+	// validate the list
+	if err := MinItems(10)(testList); err == nil {
+		t.Error("No error returned with input less than 10 items.")
+	}
 }
 
 func TestMaxLength(t *testing.T) {
