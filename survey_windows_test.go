05d3f2d5a2a2f50c50cdc3b25d4496774d52da95
Mislav Marohnić <git@mislav.net>
2022-01-27 13:12:55 +0100

    Fix Windows imports

--- survey_windows_test.go
+++ survey_windows_test.go
@@ -4,7 +4,6 @@ import (
 	"testing"
 
 	"github.com/AlecAivazis/survey/v2/terminal"
-	expect "github.com/Netflix/go-expect"
 )
 
 func RunTest(t *testing.T, procedure func(expectConsole), test func(terminal.Stdio) error) {
