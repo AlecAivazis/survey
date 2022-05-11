f324390c34cda154da6053b9b511394df453843a
luanraithz <luanraithz@gmail.com>
2022-05-11 17:03:47 -0300

    Review

--- examples/select_description.go
+++ examples/select_description.go
@@ -19,35 +19,29 @@ func main() {
 		{Title: "Burger", Comment: "Really?"},
 	}
 
-	answers := struct {
-		Meal string
-	}{}
-
 	titles := make([]string, len(meals))
 	for i, m := range meals {
 		titles[i] = m.Title
 	}
-	var qs = []*survey.Question{
-		{
-			Name: "meal",
-			Prompt: &survey.Select{
-				Message: "Choose a meal:",
-				Options: titles,
-				Description: func(value string, index int) string {
-					return meals[index].Comment
-				},
-			},
-			Validate: survey.Required,
+	var qs = &survey.Select{
+		Message: "Choose a meal:",
+		Options: titles,
+		Description: func(value string, index int) string {
+			return meals[index].Comment
 		},
 	}
 
+	answerIndex := 0
+
 	// ask the question
-	err := survey.Ask(qs, &answers)
+	err := survey.AskOne(qs, &answerIndex)
 
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
+
+	meal := meals[answerIndex]
 	// print the answers
-	fmt.Printf("you picked %s, nice choice.\n", answers.Meal)
+	fmt.Printf("you picked %s, nice choice.\n", meal.Title)
 }
