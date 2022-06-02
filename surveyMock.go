dc306a6fda3371a29fae32f669b350c958037759
Jakob Gerstmayer <jakob.gerstmayer@mdctec.com>
2022-06-02 18:04:53 +0200

    Added a simple mock struct that always returns nil

---
+++ surveyMock.go
@@ -0,0 +1,41 @@
+package survey
+
+import (
+	"io"
+
+	"github.com/AlecAivazis/survey/v2/terminal"
+)
+
+
+type SurveyMock struct{}
+
+//TODO implement the mock functionality
+func (survey *SurveyMock) WithStdio(in terminal.FileReader, out terminal.FileWriter, err io.Writer) AskOpt {
+	return nil
+}
+func (survey *SurveyMock) WithFilter(filter func(filter string, value string, index int) (include bool)) AskOpt {
+	return nil
+}
+func (survey *SurveyMock) WithKeepFilter(KeepFilter bool) AskOpt {
+	return nil
+}
+func (survey *SurveyMock) WithValidator(v Validator) AskOpt {
+	return nil
+}
+func (survey *SurveyMock) WithPageSize(pageSize int) AskOpt {
+	return nil
+}
+func (survey *SurveyMock) WithHelpInput(r rune) AskOpt {
+	return nil
+}
+func (survey *SurveyMock) WithIcons(setIcons func(*IconSet)) AskOpt {
+	return nil
+}
+func (survey *SurveyMock) WithShowCursor(ShowCursor bool) AskOpt {
+	return nil
+}
+func (survey *SurveyMock) AskOne(p Prompt, response interface{}, opts ...AskOpt) error {
+	return nil
+func (survey *SurveyMock) Ask(qs []*Question, response interface{}, opts ...AskOpt) error {
+	return nil
+}
