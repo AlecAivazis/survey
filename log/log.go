2de61ae8b13f5ec16780d2cb0b26087eaba95d7e
Mislav Marohnić <git@mislav.net>
2022-11-24 19:15:19 +0100

    Make logging opt-in

--- log/log.go
+++ log/log.go
@@ -1,24 +1,41 @@
 package log
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"strings"
 )
 
+var enabled bool
+
 func init() {
-	w, err := os.OpenFile("survey.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		panic(err)
+	logFile := os.Getenv("SURVEY_LOG_FILE")
+	if logFile == "" {
+		return
 	}
+
+	w, err := os.OpenFile(logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "survey: enable to open file %q for logging\n", logFile)
+		return
+	}
+
 	log.SetOutput(w)
 	log.Print(strings.Repeat("=", 80))
+	enabled = true
 }
 
 func Print(s string) {
+	if !enabled {
+		return
+	}
 	log.Print(s)
 }
 
 func Printf(f string, args ...interface{}) {
+	if !enabled {
+		return
+	}
 	log.Printf(f, args...)
 }
