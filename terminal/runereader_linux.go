5c6f589be0761ce84d57786a76dba483be7198df
zhangyd <zhangyd@keyou.cn>
2023-09-01 15:18:49 +0800

    support mips64le

--- terminal/runereader_linux.go
+++ terminal/runereader_linux.go
@@ -2,8 +2,8 @@
 // Copyright 2013 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-//go:build linux && !ppc64le
-// +build linux,!ppc64le
+//go:build linux && !ppc64le && !mips64le
+// +build linux,!ppc64le,!mips64le
 
 package terminal
 
